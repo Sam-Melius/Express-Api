a59de21247fcd85eff21151e90e56faa923054eb
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-03-15 14:53:47 -0700

    added routes in app.js and wrote first test for create

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const monsters = require('../lib/models/Monster');
 
 describe('Express-Api routes', () => {
   beforeEach(() => {
@@ -11,4 +12,15 @@ describe('Express-Api routes', () => {
   afterAll(() => {
     pool.end();
   });
+});
+
+it('creates a monster', async () => {
+  const expected = {
+    name: 'Blubber',
+    type: 'Goo',
+    size: 2,
+  };
+  const res = await (await request(app).post('/api/v1/monsters')).send(expected);
+
+  expect(res.body).toEqual({ id: expect.any(String), ...expected });
 });
