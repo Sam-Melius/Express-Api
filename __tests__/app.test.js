ef774d7c2764778693f1cdbdf7e5ec4292881256
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-03-15 15:53:07 -0700

    wrote insert and test is pass but test suite is failing

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,7 +2,7 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
-const monsters = require('../lib/models/Monster');
+const Monster = require('../lib/models/Monster');
 
 describe('Express-Api routes', () => {
   beforeEach(() => {
@@ -20,7 +20,7 @@ it('creates a monster', async () => {
     type: 'Goo',
     size: 2,
   };
-  const res = await (await request(app).post('/api/v1/monsters')).send(expected);
+  const res = await request(app).post('/api/v1/monsters').send(expected);
 
   expect(res.body).toEqual({ id: expect.any(String), ...expected });
 });
